3b0cf54a08f515abfd8ed9188ffbdb0e488380ee
Rdroid-101 <raggav.subramani@gmail.com>
2022-02-13 18:00:39 +0530

    initial commit

---
+++ Question1.java
@@ -0,0 +1,11 @@
+// Use an array to solve the weather problem.
+// Get the input from the user for the city you live currently, the temperature
+// for last 2 weeks in the month January 2022 (14 days). Write a java program
+// to find the 3 hottest days, 3 coldest day and Average for the month. Print
+// the values with the city details.
+// Optional: Use math.Random Values ranging from 25 to 45 for
+// temperature values.
+
+public class Question1 {
+    
+}
---
+++ Question2.java
@@ -0,0 +1,13 @@
+// Write a java program to create a class called TotalGoalsScored that
+// represents the total number of goals scored by a football team. The
+// TotalGoalsScored class should contain a single integer as instance data,
+// representing the number of goals scored. Write a constructor to initialize
+// the number of goals to zero. Write a method called setGoal that increments
+// the value by one whenever a goal is scored, and another method called
+// getGoal that returns the total number of goals scored so far. Finally, create
+// a driver class called GoalTracker that creates a few TotalGoalsScored
+// objects and tests their methods.
+
+public class Question2 {
+
+}
